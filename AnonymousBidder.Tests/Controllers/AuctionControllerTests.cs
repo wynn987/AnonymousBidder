cbacb249361f72fdb74a8e4ad40a0c9e287ec05e
wynn <wynn987@gmail.com>
2017-09-18 15:06:42 +0800

    Not sure what i added actually

---
+++ AnonymousBidder.Tests/Controllers/AuctionControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnonymousBidder.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace AnonymousBidder.Controllers.Tests
+{
+    [TestClass()]
+    public class AuctionControllerTests
+    {
+        [TestMethod]
+        public void AuctionItemViewTest()
+        {
+            var controller = new AuctionController();
+            var result = controller.Item() as ViewResult;
+            Assert.AreEqual("Item", result.ViewName);
+        }
+    }
+}
---
+++ AnonymousBidder.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnonymousBidder.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnonymousBidder.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee772225-4ce1-488f-a641-186c06560c24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
