1dd4cfbb7f0c61fc05f7fc2d99e98fd7841eae54
wynn <wynn987@gmail.com>
2017-10-28 20:07:55 +0800

    Fixed foreign key

--- AnonymousBidder/Data/Entity/Bid.cs
+++ AnonymousBidder/Data/Entity/Bid.cs
@@ -9,15 +9,15 @@ namespace AnonymousBidder.Data.Entity
 {
     public class Bid : BaseEntity
     {
-        [Required]
+        [Key, Required, DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid BidGUID { get; set; }
         [Required, Range(0, 9999999999.99)]
         public decimal BidPlaced { get; set; }
         [Required]
-        [Key, ForeignKey("Auction")]
         public Guid Bid_AuctionGUID{ get; set; }
         public Guid? Bid_ABUserGUID { get; set; }
 
+        [ForeignKey("Bid_AuctionGUID")]
         public virtual Auction Auction { get; set; }
         [ForeignKey("Bid_ABUserGUID")]
         public virtual ABUser Bidder { get; set; }
