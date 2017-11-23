72eb3e61ff747d6d0f52c9c2a8ae2313ac2fad8b
wynn <wynn987@gmail.com>
2017-11-23 14:14:46 +0800

    Added Missing Security features

--- AnonymousBidder/Models/BidModel.cs
+++ AnonymousBidder/Models/BidModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace AnonymousBidder.Models
     public class BidModel
     {
         public Guid BidGUID { get; set; }
+        [Range(0.0, 99999999)]
         public decimal BidPlaced { get; set; }
         public Guid Bid_AuctionGUID { get; set; }
         public Guid? Bid_ABUserGUID { get; set; }
